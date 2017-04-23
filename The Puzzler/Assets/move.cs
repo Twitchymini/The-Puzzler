dbb0d2ebf9e511781f45fe6a1c10ccb1e5f83d34
Twitchymini <zook.star@hotmail.co.uk>
2017-04-23 20:21:59 +0100

    added cube

---
+++ The Puzzler/Assets/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    float m_time = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_time += Time.deltaTime;
+
+        Vector3 pos = gameObject.transform.position;
+        pos.x += Mathf.Sin(m_time);
+        gameObject.transform.position = pos;
+    }
+}
